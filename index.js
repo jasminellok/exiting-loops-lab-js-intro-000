9cadea7b1c5e7ce4c64076b831947e1e7e301c1b
Jasmine Lok <jasminellok@gmail.com>
2020-04-15 16:08:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function breakOut(array, changeValue, stopValue) {
-  for (let i=1; i<array.length; i+=1) {
+  for (let i=0; i<array.length; i+=1) {
     if (array[i] !== stopValue) {
       array[i] = changeValue
     } else {
