88f493f60e82500d59e3f41ccf06366aca32944b
Jasmine Lok <jasminellok@gmail.com>
2020-04-15 15:58:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let num = 1; num < array.length; num += 1) {
+    if (typeof array[num] !== stopValue) {
+      array[num]=changeValue[num]
+    } else {
+      break 
+    }
+  }
+}
+
