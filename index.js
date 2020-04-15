9ab27cc3404e3fc202548bcb91be971ce2ead2ec
Jasmine Lok <jasminellok@gmail.com>
2020-04-15 16:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i=0; i<array.length; i+=1) {
-    findFn
     if (findFn === true) {
       return array[i]
     } else {
